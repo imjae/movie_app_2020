a32e26c521d3a86d22c42792eecae9109f7b6dbc
jake <jakeim1111@gmail.com>
2020-05-10 15:24:22 +0900

    Updates

---
+++ precache-manifest.9d8a71d7321e31c9aa69185ea7632c36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fff3f4f94c77637ed4b3fb883668c03",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1e7caddd4d8a9080807c",
+    "url": "/movie_app_2020/static/css/main.ecd5d10d.chunk.css"
+  },
+  {
+    "revision": "261fc6083f1d5d248b8b",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e7caddd4d8a9080807c",
+    "url": "/movie_app_2020/static/js/main.553c4230.chunk.js"
+  },
+  {
+    "revision": "9927c9d5c16bc5ac0d63",
+    "url": "/movie_app_2020/static/js/runtime-main.72517e8e.js"
+  }
+]);
